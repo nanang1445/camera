8b16c6ec2aaf89211c7ba1157cad0aa5b7a0efbb
nanang1445 <nanangrezeki99@gmail.com>
2019-11-26 17:12:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/camera/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.camera2
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import android.graphics.Bitmap
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button_capture.setOnClickListener {
+            var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            startActivityForResult(i, 123)
+        }
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==123) {
+            var bmp= data?.extras?.get("data") as? Bitmap
+            imageView.setImageBitmap(bmp)
+        }
+    }
+}
